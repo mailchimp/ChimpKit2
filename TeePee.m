205c866b93d5e485f811b92b7814a2fd519efeb1
twoism <signalstatic@gmail.com>
2010-02-03 23:31:15 -0500

    #

--- TeePee.m
+++ TeePee.m
@@ -3,7 +3,7 @@
 //  ChimpKit
 //
 //  Created by Christopher Burnett on 2/3/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright 2010 twoism. All rights reserved.
 //
 
 #import "TeePee.h"
